48627dd6f1649067b509cacd678e37c3a76d9ae6
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2022-03-03 23:36:00 -0300

    installed dropzone and structured page with styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React,{ Component } from "react";
+import GlobalStyle from './styles/global';
+import {Container, Content } from './styles'
+import Upload from "./components/Upload";
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <Container>
+        <Content>
+         <Upload />
+        </Content>
+        <GlobalStyle />
+      </Container>
+  )
+  }
 }
 
 export default App;
